2c44f0701416565aa4026b482bc796aa778cd2cb
aaronMan12 <aaronhernandez597@gmail.com>
2023-11-13 23:09:16 -0600

    creación de paths empresa

--- src/java/ws/EmpresaWS.java
+++ src/java/ws/EmpresaWS.java
@@ -5,13 +5,17 @@
  */
 package ws;
 
+import javax.annotation.Priority;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -25,29 +29,41 @@ public class EmpresaWS {
     @Context
     private UriInfo context;
 
-    /**
-     * Creates a new instance of EmpresaWS
-     */
-    public EmpresaWS() {
-    }
-
-    /**
-     * Retrieves representation of an instance of ws.EmpresaWS
-     * @return an instance of java.lang.String
-     */
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * PUT method for updating or creating an instance of EmpresaWS
-     * @param content representation for the resource
-     */
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    @Path("registroEmpresa")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String registroEmpresa() {
+
+        return "put";
     }
+
+    @POST
+    @Path("edicionEmpresa/{idEmpresa}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String edicionEmpresa(@PathParam("idEmpresa") Integer idEmpresa) {
+
+        return "" + idEmpresa;
+    }
+
+    @DELETE
+    @Path("eliminarEmpresa/{idEmpresa}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String eliminarEmpresa(@PathParam("idEmpresa") Integer idEmpresa) {
+
+        return "" + idEmpresa;
+    }
+
+    @GET
+    @Path("buscarEmpresa/{parametro}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String buscarEmpresaBy_Name_RFC_REPRESENTANTENAME(@PathParam("parametro") String parametro) {
+
+        if (parametro.length() == 8) {
+            //validaciones para el tipo de consulta
+        }
+
+        return parametro;
+    }
+    
+    
 }
