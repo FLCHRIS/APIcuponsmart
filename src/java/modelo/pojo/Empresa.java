9e47e033b539c5766f2968eb4199efbc28d9a611
Oscar015114 <pereag30@gmail.com>
2023-11-13 22:12:02 -0600

    finalizar configuracion a la conexion de la base de datos

--- src/java/modelo/pojo/Empresa.java
+++ src/java/modelo/pojo/Empresa.java
@@ -10,5 +10,152 @@ package modelo.pojo;
  * @author Oscar
  */
 public class Empresa {
+    private Integer idEmpresa;
+    private String nombreComercial;
+    private byte [] logo;
+    private String nombreRepresentanteLegal;
+    private String email;
+    private String calle;
+    private Integer numero;
+    private String codigoPostal;
+    private String ciudad;
+    private String telefono;
+    private String paginaWeb;
+    private String RFC;
+    private String estatus;
+    private String logoBase64;
+
+    public Empresa() {
+    }
+
+    public Empresa(Integer idEmpresa, String nombreComercial, byte[] logo, String nombreRepresentanteLegal, String email, String calle, Integer numero, String codigoPostal, String ciudad, String telefono, String paginaWeb, String RFC, String estatus, String logoBase64) {
+        this.idEmpresa = idEmpresa;
+        this.nombreComercial = nombreComercial;
+        this.logo = logo;
+        this.nombreRepresentanteLegal = nombreRepresentanteLegal;
+        this.email = email;
+        this.calle = calle;
+        this.numero = numero;
+        this.codigoPostal = codigoPostal;
+        this.ciudad = ciudad;
+        this.telefono = telefono;
+        this.paginaWeb = paginaWeb;
+        this.RFC = RFC;
+        this.estatus = estatus;
+        this.logoBase64 = logoBase64;
+    }
+
+    public Integer getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(Integer idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombreComercial() {
+        return nombreComercial;
+    }
+
+    public void setNombreComercial(String nombreComercial) {
+        this.nombreComercial = nombreComercial;
+    }
+
+    public byte[] getLogo() {
+        return logo;
+    }
+
+    public void setLogo(byte[] logo) {
+        this.logo = logo;
+    }
+
+    public String getNombreRepresentanteLegal() {
+        return nombreRepresentanteLegal;
+    }
+
+    public void setNombreRepresentanteLegal(String nombreRepresentanteLegal) {
+        this.nombreRepresentanteLegal = nombreRepresentanteLegal;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPaginaWeb() {
+        return paginaWeb;
+    }
+
+    public void setPaginaWeb(String paginaWeb) {
+        this.paginaWeb = paginaWeb;
+    }
+
+    public String getRFC() {
+        return RFC;
+    }
+
+    public void setRFC(String RFC) {
+        this.RFC = RFC;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getLogoBase64() {
+        return logoBase64;
+    }
+
+    public void setLogoBase64(String logoBase64) {
+        this.logoBase64 = logoBase64;
+    }
+    
     
 }
--- src/java/ws/EmpresaWS.java
+++ src/java/ws/EmpresaWS.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 
+
 /**
  * REST Web Service
  *
@@ -38,14 +39,10 @@ public class EmpresaWS {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+       return "hola";
     }
 
-    /**
-     * PUT method for updating or creating an instance of EmpresaWS
-     * @param content representation for the resource
-     */
+    
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(String content) {
