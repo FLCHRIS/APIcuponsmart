43633f27bf2b9bbcfd517408b0bb5707d6ae76f5
Christian <filetelopezchristian@gmail.com>
2023-11-15 12:15:38 -0600

    Se creó el método de agregar, (aún no está terminado)

--- src/java/modelo/EmpresaDAO.java
+++ src/java/modelo/EmpresaDAO.java
@@ -5,10 +5,36 @@
  */
 package modelo;
 
+import modelo.pojo.Mensaje;
+import mybatis.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+
 /**
  *
  * @author Oscar
  */
 public class EmpresaDAO {
-    
+    public static Mensaje registrar() {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+        
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+            
+            int filasAfectadas = conexionDB.insert("empresa.registrar", "XXX");
+            conexionDB.commit();
+            
+            if (filasAfectadas > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Empresa registrada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo registrar la empresa");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+        return mensaje;
+    }
 }
