4043207937931cc449b25ed7a23d0839b903ee95
Christian <filetelopezchristian@gmail.com>
2023-11-15 23:07:37 -0600

    Se implementó el apartado de registrar empresa

--- src/java/modelo/EmpresaDAO.java
+++ src/java/modelo/EmpresaDAO.java
@@ -1,29 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
+import modelo.pojo.Empresa;
 import modelo.pojo.Mensaje;
 import mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
-/**
- *
- * @author Oscar
- */
 public class EmpresaDAO {
-    public static Mensaje registrar() {
+    public static Mensaje registrar(
+            String nombreComercial, String nombreRepresentanteLegal, String email, 
+            String calle, Integer numero, String codigoPostal, String ciudad, String telefono, 
+            String paginaWeb, String RFC) {
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
+        
+        Empresa nuevaEmpresa = new Empresa();
+        nuevaEmpresa.setNombreComercial(nombreComercial);
+        // nuevaEmpresa.setLogo(logo); , byte[] logo
+        nuevaEmpresa.setNombreRepresentanteLegal(nombreRepresentanteLegal);
+        nuevaEmpresa.setEmail(email);
+        nuevaEmpresa.setCalle(calle);
+        nuevaEmpresa.setNumero(numero);
+        nuevaEmpresa.setCodigoPostal(codigoPostal);
+        nuevaEmpresa.setCiudad(ciudad);
+        nuevaEmpresa.setTelefono(telefono);
+        nuevaEmpresa.setPaginaWeb(paginaWeb);
+        nuevaEmpresa.setRFC(RFC);
         
         try (SqlSession conexionDB = MyBatisUtil.getSession()) {
             if (conexionDB == null) {
                 mensaje.setContenido("No hay conexión a la base de datos");
             }
             
-            int filasAfectadas = conexionDB.insert("empresa.registrar", "XXX");
+            int filasAfectadas = conexionDB.insert("empresa.registrar", nuevaEmpresa);
             conexionDB.commit();
             
             if (filasAfectadas > 0) {
