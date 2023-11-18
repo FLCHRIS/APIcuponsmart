5ea62b657ad377884eb2b24df8b45fc7cc5fcb09
Christian <filetelopezchristian@gmail.com>
2023-11-17 23:36:21 -0600

    Se arreglo el proyecto en general

---
+++ src/java/modelo/pojo/RepresentanteLegal.java
@@ -0,0 +1,51 @@
+package modelo.pojo;
+
+public class RepresentanteLegal {
+    private Integer idRepresentanteLegal;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+
+    public RepresentanteLegal() {
+    }
+    
+    public RepresentanteLegal(Integer idRepresentanteLegal, String nombre, String apellidoPaterno, String apellidoMaterno) {
+        this.idRepresentanteLegal = idRepresentanteLegal;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public Integer getIdRepresentanteLegal() {
+        return idRepresentanteLegal;
+    }
+
+    public void setIdRepresentanteLegal(Integer idRepresentanteLegal) {
+        this.idRepresentanteLegal = idRepresentanteLegal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    
+}
