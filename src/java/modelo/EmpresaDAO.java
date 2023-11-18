81cd9421951c8637153872433e827eb59203eae5
Oscar015114 <pereag30@gmail.com>
2023-11-17 23:08:42 -0600

    se creo buscarPorNombreEmpresa

--- src/java/modelo/EmpresaDAO.java
+++ src/java/modelo/EmpresaDAO.java
@@ -9,20 +9,20 @@ import mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
 public class EmpresaDAO {
-    
+
     public static Mensaje obtenerEmpresaPorRFC(String RFC) {
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
-        
-        try (SqlSession conexionDB = MyBatisUtil.getSession()){
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
             if (conexionDB == null) {
                 mensaje.setContenido("No hay conexion con la base de datos");
                 return mensaje;
             }
-            
+
             List<Empresa> empresas = conexionDB.selectList("empresa.obtenerEmpresaPorRFC", RFC);
             mensaje.setEmpresas(empresas);
-            
+
             if (!empresas.isEmpty()) {
                 mensaje.setError(Boolean.FALSE);
                 mensaje.setContenido("Respuesta exitosa");
@@ -32,16 +32,16 @@ public class EmpresaDAO {
         } catch (Exception e) {
             mensaje.setContenido("Error: " + e.getMessage());
         }
-        
+
         return mensaje;
     }
-    
+
     public static Mensaje registrar(
-            String nombre, String nombreComercial, String email, String telefono, 
+            String nombre, String nombreComercial, String email, String telefono,
             String paginaWeb, String RFC) {
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
-        
+
         Empresa nuevaEmpresa = new Empresa();
         nuevaEmpresa.setNombreComercial(nombreComercial);
         // nuevaEmpresa.setLogo(logo); , byte[] logo
@@ -50,15 +50,15 @@ public class EmpresaDAO {
         nuevaEmpresa.setTelefono(telefono);
         nuevaEmpresa.setPaginaWeb(paginaWeb);
         nuevaEmpresa.setRFC(RFC);
-        
+
         try (SqlSession conexionDB = MyBatisUtil.getSession()) {
             if (conexionDB == null) {
                 mensaje.setContenido("No hay conexión a la base de datos");
             }
-            
+
             int filasAfectadas = conexionDB.insert("empresa.registrar", nuevaEmpresa);
             conexionDB.commit();
-            
+
             if (filasAfectadas > 0) {
                 mensaje.setError(Boolean.FALSE);
                 mensaje.setContenido("Empresa registrada con éxito");
@@ -70,32 +70,32 @@ public class EmpresaDAO {
         }
         return mensaje;
     }
-    
+
     public static Mensaje registrarRepresentante(
             String nombre, String apellidoPaterno, String apellidoMaterno, Integer idEmpresa) {
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
-        
+
         RepresentanteLegal representanteLegal = new RepresentanteLegal();
         representanteLegal.setNombre(nombre);
         representanteLegal.setApellidoPaterno(apellidoPaterno);
         representanteLegal.setApellidoMaterno(apellidoMaterno);
-        
+
         try (SqlSession conexionDB = MyBatisUtil.getSession()) {
             if (conexionDB == null) {
                 mensaje.setContenido("No hay conexión a la base de datos");
             }
-            
+
             int filasRegistroRepresentanteAfectadas = conexionDB.insert("empresa.registrarRepresentanteLegal", representanteLegal);
             int idRepresentanteLegal = representanteLegal.getIdRepresentanteLegal();
-            
+
             Empresa empresa = new Empresa();
             empresa.setIdEmpresa(idEmpresa);
             empresa.setIdRepresentanteLegal(idRepresentanteLegal);
-            
+
             int filasRegistroRepresentanteEmpresa = conexionDB.update("empresa.registrarRepresentanteAEmpresa", empresa);
             conexionDB.commit();
-            
+
             if (filasRegistroRepresentanteAfectadas > 0 && filasRegistroRepresentanteEmpresa > 0) {
                 mensaje.setError(Boolean.FALSE);
                 mensaje.setContenido("Representante registrado con éxito");
@@ -107,27 +107,27 @@ public class EmpresaDAO {
         }
         return mensaje;
     }
-    
+
     public static Mensaje editarRepresentante(
             String nombre, String apellidoPaterno, String apellidoMaterno, Integer idRepresentanteLegal) {
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
-        
+
         RepresentanteLegal representanteLegal = new RepresentanteLegal();
         representanteLegal.setNombre(nombre);
         representanteLegal.setApellidoPaterno(apellidoPaterno);
         representanteLegal.setApellidoMaterno(apellidoMaterno);
         representanteLegal.setIdRepresentanteLegal(idRepresentanteLegal);
-        
+
         try (SqlSession conexionDB = MyBatisUtil.getSession()) {
             if (conexionDB == null) {
                 mensaje.setContenido("No hay conexión a la base de datos");
             }
-            
+
             int filasAfectadas = conexionDB.update("empresa.editarRepresentanteLegal", representanteLegal);
             conexionDB.commit();
-            
-            if (filasAfectadas > 0 ) {
+
+            if (filasAfectadas > 0) {
                 mensaje.setError(Boolean.FALSE);
                 mensaje.setContenido("Representante actualizado con éxito");
             } else {
@@ -138,13 +138,13 @@ public class EmpresaDAO {
         }
         return mensaje;
     }
-    
+
     public static Mensaje editarEmpresa(
-            String nombre, String nombreComercial, String email, String telefono, 
+            String nombre, String nombreComercial, String email, String telefono,
             String paginaWeb, String estatus, Integer idEmpresa) {
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
-        
+
         HashMap<String, Object> editarEmpresa = new HashMap<>();
         editarEmpresa.put("idEmpresa", idEmpresa);
         editarEmpresa.put("nombre", nombre);
@@ -153,15 +153,15 @@ public class EmpresaDAO {
         editarEmpresa.put("telefono", telefono);
         editarEmpresa.put("paginaWeb", paginaWeb);
         editarEmpresa.put("estatus", estatus);
-        
+
         try (SqlSession conexionDB = MyBatisUtil.getSession()) {
             if (conexionDB == null) {
                 mensaje.setContenido("No hay conexión a la base de datos");
             }
-            
+
             int filasAfectadas = conexionDB.update("empresa.editarEmpresa", editarEmpresa);
             conexionDB.commit();
-            
+
             if (filasAfectadas > 0) {
                 mensaje.setError(Boolean.FALSE);
                 mensaje.setContenido("Empresa actualizada con éxito");
@@ -184,7 +184,7 @@ public class EmpresaDAO {
                 Empresa empresa = new Empresa();
                 empresa.setIdEmpresa(idEmpresa);
                 empresa.setLogo(logo);
-                
+
                 int filasAfectadas = conexionBD.update("empresa.subirLogo", empresa);
                 conexionBD.commit();
 
@@ -203,6 +203,33 @@ public class EmpresaDAO {
             mensaje.setContenido("Lo sentimos, no hay conexión para guardar el logo.");
         }
 
+        return mensaje;
+    }
+
+    public static Mensaje buscarEmpresaPorNombre(String nombre) {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if (conexionBD != null) {
+            try {
+                List<Empresa> empresas
+                        = conexionBD.selectList("empresa.obtenerEmpresaPorNombre", nombre);
+
+                if (!empresas.isEmpty()) {
+                    mensaje.setError(false);
+                    mensaje.setContenido("Respuesta exitosa");
+                    mensaje.setEmpresas(empresas);
+                } else {
+                    mensaje.setContenido("No se encontro ninguna empresa con el nombre seleccionado");
+                }
+            } catch (Exception e) {
+                mensaje.setContenido("Error: " + e.getMessage());
+            } finally {
+                conexionBD.close();
+            }
+        } else {
+            mensaje.setContenido("Error: Por el momento no hay conexion en la base de datos");
+        }
         return mensaje;
     }
 }
