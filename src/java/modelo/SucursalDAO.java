0f932757deab96568ea1507d9193d8dbaf223815
aaronMan12 <aaronhernandez597@gmail.com>
2023-11-18 15:04:00 -0600

    mas cambios sucursal-ubicacion

--- src/java/modelo/SucursalDAO.java
+++ src/java/modelo/SucursalDAO.java
@@ -1,12 +1,14 @@
 package modelo;
 
 import modelo.pojo.Mensaje;
+import modelo.pojo.RepresentanteLegal;
 import modelo.pojo.Sucursal;
+import modelo.pojo.Ubicacion;
 import org.apache.ibatis.session.SqlSession;
 
 public class SucursalDAO {
-    
-    public static Mensaje agregarSucursal(int idEmpresa, String nombre, String telefono, float latitud, float longitud){
+
+    public static Mensaje agregarSucursal(int idEmpresa, String nombre, String telefono, float latitud, float longitud) {
         Mensaje msj = new Mensaje();
         msj.setError(Boolean.TRUE);
         SqlSession conexionBD = mybatis.MyBatisUtil.getSession();
@@ -17,24 +19,45 @@ public class SucursalDAO {
         sucursal.setTelefono(telefono);
         sucursal.setLatitud(latitud);
         sucursal.setLongitud(longitud);
-        
-        if (conexionBD != null){
+
+        if (conexionBD != null) {
             try {
                 int filasAfectadas = conexionBD.insert("sucursal.registrar", sucursal);
                 conexionBD.commit();
-                if (filasAfectadas != 0){
+                if (filasAfectadas != 0) {
                     msj.setError(Boolean.FALSE);
-                    msj.setContenido("La sucursal "+sucursal.getNombre()+" esta dada de alta");
-                }else{
+                    msj.setContenido("La sucursal " + sucursal.getNombre() + " esta dada de alta");
+                } else {
                     msj.setContenido("Error al agregar la sucursal");
                 }
             } catch (Exception e) {
-                    e.printStackTrace();
-                    msj.setContenido("Error de compilacion: "+ e);
-            }finally{
+                e.printStackTrace();
+                msj.setContenido("Error de compilacion: " + e);
+            } finally {
                 conexionBD.close();
             }
         }
-        return msj;   
+        return msj;
+    }
+    
+    public static Mensaje agregarUbicación(String calle, int numero, String codigoPostal, String ciudad){
+        Mensaje msj = new Mensaje();
+        msj.setError(Boolean.TRUE);
+        SqlSession conexionBD = mybatis.MyBatisUtil.getSession();
+            Ubicacion ubicacion = new Ubicacion();
+            ubicacion.setCalle(calle);
+            ubicacion.setNumero(numero);
+            ubicacion.setCodigoPostal(codigoPostal);
+            ubicacion.setCiudad(ciudad);
+       
+        if (conexionBD != null){
+            try {
+                
+            } catch (Exception e) {
+            }finally{
+            }
+        }
+       
+    return msj;
     }
 }
