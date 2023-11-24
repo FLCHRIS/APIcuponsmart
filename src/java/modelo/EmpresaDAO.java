e1513bfd041ce74ccd51e33589fd47143afdca92
Christian <filetelopezchristian@gmail.com>
2023-11-24 11:20:41 -0600

    Se implementó el método de eliminar representante legal y ubicación. Ademas de ordenar los métodos

--- src/java/modelo/EmpresaDAO.java
+++ src/java/modelo/EmpresaDAO.java
@@ -11,7 +11,107 @@ import org.apache.ibatis.session.SqlSession;
 
 public class EmpresaDAO {
 
-    public static Mensaje buscarEmpresaPorNombre(String nombre) {
+    /* 
+    #########################
+    ######## EMPRESA ########
+    #########################    
+     */
+    public static Mensaje registrar(
+            String nombre, String nombreComercial, String email, String telefono,
+            String paginaWeb, String RFC) {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+
+        Empresa nuevaEmpresa = new Empresa();
+        nuevaEmpresa.setNombreComercial(nombreComercial);
+        nuevaEmpresa.setNombre(nombre);
+        nuevaEmpresa.setEmail(email);
+        nuevaEmpresa.setTelefono(telefono);
+        nuevaEmpresa.setPaginaWeb(paginaWeb);
+        nuevaEmpresa.setRFC(RFC);
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+
+            int filasAfectadas = conexionDB.insert("empresa.registrar", nuevaEmpresa);
+            conexionDB.commit();
+
+            if (filasAfectadas > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Empresa registrada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo registrar la empresa");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+        return mensaje;
+    }
+
+    public static Mensaje editarEmpresa(
+            String nombre, String nombreComercial, String email, String telefono,
+            String paginaWeb, String estatus, Integer idEmpresa) {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+
+        HashMap<String, Object> editarEmpresa = new HashMap<>();
+        editarEmpresa.put("idEmpresa", idEmpresa);
+        editarEmpresa.put("nombre", nombre);
+        editarEmpresa.put("nombreComercial", nombreComercial);
+        editarEmpresa.put("email", email);
+        editarEmpresa.put("telefono", telefono);
+        editarEmpresa.put("paginaWeb", paginaWeb);
+        editarEmpresa.put("estatus", estatus);
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+
+            int filasAfectadas = conexionDB.update("empresa.editarEmpresa", editarEmpresa);
+            conexionDB.commit();
+
+            if (filasAfectadas > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Empresa actualizada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo actualizar la empresa");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+        return mensaje;
+    }
+
+    public static Mensaje eliminarEmpresa(int idEmpresa) {
+        Mensaje msj = new Mensaje();
+        msj.setError(Boolean.TRUE);
+        SqlSession conexinBD = mybatis.MyBatisUtil.getSession();
+
+        if (conexinBD != null) {
+            try {
+                int numeroAfectadas = conexinBD.delete("empresa.eliminar", idEmpresa);
+                conexinBD.commit();
+                if (numeroAfectadas != 0) {
+                    msj.setError(Boolean.FALSE);
+                    msj.setContenido("Empresa eliminada con éxito");
+
+                } else {
+                    msj.setContenido("Hubo un error en la operación de eliminar la empresa");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                msj.setContenido("Erroe de tipo: " + e);
+            } finally {
+                conexinBD.close();
+            }
+        }
+        return msj;
+    }
+
+    public static Mensaje obtenerEmpresaPorNombre(String nombre) {
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
 
@@ -95,33 +195,75 @@ public class EmpresaDAO {
         return mensaje;
     }
 
-    public static Mensaje registrar(
-            String nombre, String nombreComercial, String email, String telefono,
-            String paginaWeb, String RFC) {
+    public static Mensaje subirLogo(Integer idEmpresa, byte[] logo) {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+        SqlSession conexionBD = MyBatisUtil.getSession();
+
+        if (conexionBD != null) {
+            try {
+                Empresa empresa = new Empresa();
+                empresa.setIdEmpresa(idEmpresa);
+                empresa.setLogo(logo);
+
+                int filasAfectadas = conexionBD.update("empresa.subirLogo", empresa);
+                conexionBD.commit();
+
+                if (filasAfectadas > 0) {
+                    mensaje.setError(false);
+                    mensaje.setContenido("Logo de la empresa guardado correctamente");
+                } else {
+                    mensaje.setContenido("Hubo un error al intentar guardar el logo de la empresa");
+                }
+            } catch (Exception e) {
+                mensaje.setContenido("Error: " + e.getMessage());
+            } finally {
+                conexionBD.close();
+            }
+        } else {
+            mensaje.setContenido("Lo sentimos, no hay conexión para guardar el logo.");
+        }
+
+        return mensaje;
+    }
+
+    /* 
+    ###########################
+    ######## UBICACIÓN ########
+    ###########################  
+     */
+    public static Mensaje registrarUbicacion(
+            String calle, Integer numero, String codigoPostal,
+            String ciudad, Integer idEmpresa) {
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
 
-        Empresa nuevaEmpresa = new Empresa();
-        nuevaEmpresa.setNombreComercial(nombreComercial);
-        nuevaEmpresa.setNombre(nombre);
-        nuevaEmpresa.setEmail(email);
-        nuevaEmpresa.setTelefono(telefono);
-        nuevaEmpresa.setPaginaWeb(paginaWeb);
-        nuevaEmpresa.setRFC(RFC);
+        Ubicacion ubicacionEmpresa = new Ubicacion();
+        ubicacionEmpresa.setCalle(calle);
+        ubicacionEmpresa.setNumero(numero);
+        ubicacionEmpresa.setCodigoPostal(codigoPostal);
+        ubicacionEmpresa.setCiudad(ciudad);
 
         try (SqlSession conexionDB = MyBatisUtil.getSession()) {
             if (conexionDB == null) {
                 mensaje.setContenido("No hay conexión a la base de datos");
             }
 
-            int filasAfectadas = conexionDB.insert("empresa.registrar", nuevaEmpresa);
+            int filasRegistroUbicacion = conexionDB.insert("ubicacion.registrar", ubicacionEmpresa);
+            int idUbicacion = ubicacionEmpresa.getIdUbicacion();
+
+            Empresa empresa = new Empresa();
+            empresa.setIdEmpresa(idEmpresa);
+            empresa.setIdUbicacion(idUbicacion);
+
+            int filasRegistroUbicacionEmpresa = conexionDB.update("ubicacion.registrarUbicacionAEmpresa", empresa);
             conexionDB.commit();
 
-            if (filasAfectadas > 0) {
+            if (filasRegistroUbicacion > 0 && filasRegistroUbicacionEmpresa > 0) {
                 mensaje.setError(Boolean.FALSE);
-                mensaje.setContenido("Empresa registrada con éxito");
+                mensaje.setContenido("Ubicación registrada con éxito");
             } else {
-                mensaje.setContenido("No se pudo registrar la empresa");
+                mensaje.setContenido("No se pudo registrar la ubicación");
             }
         } catch (Exception e) {
             mensaje.setContenido("Error: " + e.getMessage());
@@ -129,6 +271,71 @@ public class EmpresaDAO {
         return mensaje;
     }
 
+    public static Mensaje editarUbicacion(
+            String calle, Integer numero, String codigoPostal,
+            String ciudad, Integer idUbicacion) {
+
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+
+        Ubicacion ubicacion = new Ubicacion();
+        ubicacion.setCalle(calle);
+        ubicacion.setNumero(numero);
+        ubicacion.setCodigoPostal(codigoPostal);
+        ubicacion.setCiudad(ciudad);
+        ubicacion.setIdUbicacion(idUbicacion);
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+
+            int filasAfectadas = conexionDB.update("ubicacion.editar", ubicacion);
+            conexionDB.commit();
+
+            if (filasAfectadas > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Ubicación actualizada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo actualizar la ubicación");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+        return mensaje;
+    }
+
+    public static Mensaje eliminarUbicacion(Integer idUbicacion) {
+
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+
+            int filasAfectadas = conexionDB.delete("ubicacion.eliminar", idUbicacion);
+            conexionDB.commit();
+
+            if (filasAfectadas > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Ubicación eliminada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo eliminar la ubicación");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+
+        return mensaje;
+    }
+
+    /* 
+    #####################################
+    ######## REPRESENTANTE LEGAL ########
+    #####################################
+     */
     public static Mensaje registrarRepresentante(
             String nombre, String apellidoPaterno, String apellidoMaterno, Integer idEmpresa) {
         Mensaje mensaje = new Mensaje();
@@ -197,170 +404,30 @@ public class EmpresaDAO {
         return mensaje;
     }
 
-    public static Mensaje editarEmpresa(
-            String nombre, String nombreComercial, String email, String telefono,
-            String paginaWeb, String estatus, Integer idEmpresa) {
+    public static Mensaje eliminarRepresentanteLegal(Integer idRepresentanteLegal) {
+
         Mensaje mensaje = new Mensaje();
         mensaje.setError(Boolean.TRUE);
-
-        HashMap<String, Object> editarEmpresa = new HashMap<>();
-        editarEmpresa.put("idEmpresa", idEmpresa);
-        editarEmpresa.put("nombre", nombre);
-        editarEmpresa.put("nombreComercial", nombreComercial);
-        editarEmpresa.put("email", email);
-        editarEmpresa.put("telefono", telefono);
-        editarEmpresa.put("paginaWeb", paginaWeb);
-        editarEmpresa.put("estatus", estatus);
 
         try (SqlSession conexionDB = MyBatisUtil.getSession()) {
             if (conexionDB == null) {
                 mensaje.setContenido("No hay conexión a la base de datos");
             }
 
-            int filasAfectadas = conexionDB.update("empresa.editarEmpresa", editarEmpresa);
+            int filasAfectadas = conexionDB.delete("empresa.eliminarRepresentanteLegal", idRepresentanteLegal);
             conexionDB.commit();
 
             if (filasAfectadas > 0) {
                 mensaje.setError(Boolean.FALSE);
-                mensaje.setContenido("Empresa actualizada con éxito");
+                mensaje.setContenido("Representante legal eliminado con éxito");
             } else {
-                mensaje.setContenido("No se pudo actualizar la empresa");
+                mensaje.setContenido("No se pudo eliminar el representante legal");
             }
         } catch (Exception e) {
             mensaje.setContenido("Error: " + e.getMessage());
         }
-        return mensaje;
-    }
-
-    public static Mensaje subirLogo(Integer idEmpresa, byte[] logo) {
-        Mensaje mensaje = new Mensaje();
-        mensaje.setError(Boolean.TRUE);
-        SqlSession conexionBD = MyBatisUtil.getSession();
-
-        if (conexionBD != null) {
-            try {
-                Empresa empresa = new Empresa();
-                empresa.setIdEmpresa(idEmpresa);
-                empresa.setLogo(logo);
-
-                int filasAfectadas = conexionBD.update("empresa.subirLogo", empresa);
-                conexionBD.commit();
-
-                if (filasAfectadas > 0) {
-                    mensaje.setError(false);
-                    mensaje.setContenido("Logo de la empresa guardado correctamente");
-                } else {
-                    mensaje.setContenido("Hubo un error al intentar guardar el logo de la empresa");
-                }
-            } catch (Exception e) {
-                mensaje.setContenido("Error: " + e.getMessage());
-            } finally {
-                conexionBD.close();
-            }
-        } else {
-            mensaje.setContenido("Lo sentimos, no hay conexión para guardar el logo.");
-        }
 
         return mensaje;
-    }
-
-    public static Mensaje registrarUbicacion(
-            String calle, Integer numero, String codigoPostal,
-            String ciudad, Integer idEmpresa) {
-        Mensaje mensaje = new Mensaje();
-        mensaje.setError(Boolean.TRUE);
-
-        Ubicacion ubicacionEmpresa = new Ubicacion();
-        ubicacionEmpresa.setCalle(calle);
-        ubicacionEmpresa.setNumero(numero);
-        ubicacionEmpresa.setCodigoPostal(codigoPostal);
-        ubicacionEmpresa.setCiudad(ciudad);
-
-        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
-            if (conexionDB == null) {
-                mensaje.setContenido("No hay conexión a la base de datos");
-            }
-
-            int filasRegistroUbicacion = conexionDB.insert("ubicacion.registrar", ubicacionEmpresa);
-            int idUbicacion = ubicacionEmpresa.getIdUbicacion();
-
-            Empresa empresa = new Empresa();
-            empresa.setIdEmpresa(idEmpresa);
-            empresa.setIdUbicacion(idUbicacion);
-
-            int filasRegistroUbicacionEmpresa = conexionDB.update("empresa.registrarUbicacionAEmpresa", empresa);
-            conexionDB.commit();
-
-            if (filasRegistroUbicacion > 0 && filasRegistroUbicacionEmpresa > 0) {
-                mensaje.setError(Boolean.FALSE);
-                mensaje.setContenido("Ubicación registrada con éxito");
-            } else {
-                mensaje.setContenido("No se pudo registrar la ubicación");
-            }
-        } catch (Exception e) {
-            mensaje.setContenido("Error: " + e.getMessage());
-        }
-        return mensaje;
-    }
-    
-    public static Mensaje editarUbicacion(
-            String calle, Integer numero, String codigoPostal,
-            String ciudad, Integer idUbicacion) {
-        
-        Mensaje mensaje = new Mensaje();
-        mensaje.setError(Boolean.TRUE);
-        
-        Ubicacion ubicacion = new Ubicacion();
-        ubicacion.setCalle(calle);
-        ubicacion.setNumero(numero);
-        ubicacion.setCodigoPostal(codigoPostal);
-        ubicacion.setCiudad(ciudad);
-        ubicacion.setIdUbicacion(idUbicacion);
-        
-        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
-            if (conexionDB == null) {
-                mensaje.setContenido("No hay conexión a la base de datos");
-            }
-
-            int filasAfectadas = conexionDB.update("ubicacion.editar", ubicacion);
-            conexionDB.commit();
-
-            if (filasAfectadas > 0) {
-                mensaje.setError(Boolean.FALSE);
-                mensaje.setContenido("Ubicación actualizada con éxito");
-            } else {
-                mensaje.setContenido("No se pudo actualizar la ubicación");
-            }
-        } catch (Exception e) {
-            mensaje.setContenido("Error: " + e.getMessage());
-        }
-        return mensaje;
-    }
-
-    public static Mensaje eliminarEmpresa(int idEmpresa) {
-        Mensaje msj = new Mensaje();
-        msj.setError(Boolean.TRUE);
-        SqlSession conexinBD = mybatis.MyBatisUtil.getSession();
-
-        if (conexinBD != null) {
-            try {
-                int numeroAfectadas = conexinBD.delete("empresa.eliminar", idEmpresa);
-                conexinBD.commit();
-                if (numeroAfectadas != 0) {
-                    msj.setError(Boolean.FALSE);
-                    msj.setContenido("Empresa eliminada con éxito");
-
-                } else {
-                    msj.setContenido("Hubo un error en la operación de eliminar la empresa");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                msj.setContenido("Erroe de tipo: " + e);
-            } finally {
-                conexinBD.close();
-            }
-        }
-        return msj;
     }
 
 }
