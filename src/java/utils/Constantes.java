c8f42ef6b6385ef6b8042a4d319d8fd1ac5684ac
Christian <filetelopezchristian@gmail.com>
2023-11-26 19:06:07 -0600

    Se modificaron los nombrados de las constantes, además de agregar dos nuevas :nail_care:

--- src/java/utils/Constantes.java
+++ src/java/utils/Constantes.java
@@ -2,6 +2,10 @@ package utils;
 
 public class Constantes {
 
-    public static String EstadoActivo = "activo";
-    public static String EstadoInactivo = "inactivo";
+    public static String ESTADO_ACTIVO = "activo";
+    public static String ESTADO_INACTIVO = "inactivo";
+    
+    public static Integer ID_ROL_COMERCIAL = 1;
+    public static Integer ID_ROL_GENERAL = 2;
+    
 }
