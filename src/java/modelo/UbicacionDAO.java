f3baadc58e0503809f721547c18b10d397ec72df
aaronMan12 <111460458+aaronMan12@users.noreply.github.com>
2023-11-28 12:29:01 -0600

    ajuste de tabla

---
+++ src/java/modelo/UbicacionDAO.java
@@ -0,0 +1,149 @@
+package modelo;
+
+import modelo.pojo.Empresa;
+import modelo.pojo.Mensaje;
+import modelo.pojo.Sucursal;
+import modelo.pojo.Ubicacion;
+import mybatis.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+
+public class UbicacionDAO {
+
+    public static Mensaje registrarUbicacionEmpresa(
+            String calle, Integer numero, String codigoPostal,
+            String ciudad, Integer idEmpresa) {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+
+        Ubicacion ubicacionEmpresa = new Ubicacion();
+        ubicacionEmpresa.setCalle(calle);
+        ubicacionEmpresa.setNumero(numero);
+        ubicacionEmpresa.setCodigoPostal(codigoPostal);
+        ubicacionEmpresa.setCiudad(ciudad);
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+
+            int filasRegistroUbicacion = conexionDB.insert("ubicacion.registrar", ubicacionEmpresa);
+            int idUbicacion = ubicacionEmpresa.getIdUbicacion();
+
+            Empresa empresa = new Empresa();
+            empresa.setIdEmpresa(idEmpresa);
+            empresa.setIdUbicacion(idUbicacion);
+
+            int filasRegistroUbicacionEmpresa = conexionDB.update("ubicacion.registrarUbicacionAEmpresa", empresa);
+            conexionDB.commit();
+
+            if (filasRegistroUbicacion > 0 && filasRegistroUbicacionEmpresa > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Ubicación registrada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo registrar la ubicación");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+        return mensaje;
+    }
+
+    public static Mensaje registrarUbicacionSucursal(
+            String calle, Integer numero, String codigoPostal,
+            String ciudad, Integer idSucursal) {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+
+        Ubicacion ubicacionEmpresa = new Ubicacion();
+        ubicacionEmpresa.setCalle(calle);
+        ubicacionEmpresa.setNumero(numero);
+        ubicacionEmpresa.setCodigoPostal(codigoPostal);
+        ubicacionEmpresa.setCiudad(ciudad);
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+
+            int filasRegistroUbicacion = conexionDB.insert("ubicacion.registrar", ubicacionEmpresa);
+            int idUbicacion = ubicacionEmpresa.getIdUbicacion();
+
+            Sucursal sucursal = new Sucursal();
+            sucursal.setIdSucursal(idSucursal);
+            sucursal.setIdUbicacion(idUbicacion);
+
+            int filasRegistroUbicacionEmpresa = conexionDB.update("ubicacion.registrarUbicacionASucursal", sucursal);
+            conexionDB.commit();
+
+            if (filasRegistroUbicacion > 0 && filasRegistroUbicacionEmpresa > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Ubicación registrada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo registrar la ubicación");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+        return mensaje;
+    }
+    
+    public static Mensaje editarUbicacion(
+            Integer idUbicacion, String calle, Integer numero,
+            String codigoPostal, String ciudad) {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+
+        Ubicacion ubicacion = new Ubicacion();
+        ubicacion.setCalle(calle);
+        ubicacion.setNumero(numero);
+        ubicacion.setCodigoPostal(codigoPostal);
+        ubicacion.setCiudad(ciudad);
+        ubicacion.setIdUbicacion(idUbicacion);
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+
+            int filasAfectadas = conexionDB.update("ubicacion.editar", ubicacion);
+            conexionDB.commit();
+
+            if (filasAfectadas > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Ubicación actualizada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo actualizar la ubicación");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+        return mensaje;
+    }
+
+    public static Mensaje eliminarUbicacion(Integer idUbicacion) {
+
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(Boolean.TRUE);
+
+        try (SqlSession conexionDB = MyBatisUtil.getSession()) {
+            if (conexionDB == null) {
+                mensaje.setContenido("No hay conexión a la base de datos");
+            }
+
+            int filasAfectadas = conexionDB.delete("ubicacion.eliminar", idUbicacion);
+            conexionDB.commit();
+
+            if (filasAfectadas > 0) {
+                mensaje.setError(Boolean.FALSE);
+                mensaje.setContenido("Ubicación eliminada con éxito");
+            } else {
+                mensaje.setContenido("No se pudo eliminar la ubicación");
+            }
+        } catch (Exception e) {
+            mensaje.setContenido("Error: " + e.getMessage());
+        }
+
+        return mensaje;
+    }
+    
+}
---
+++ src/java/ws/UbicacionWS.java
@@ -0,0 +1,161 @@
+package ws;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import modelo.UbicacionDAO;
+import modelo.pojo.Mensaje;
+import utils.Utilidades;
+
+@Path("ubucacion")
+public class UbicacionWS {
+
+    @Context
+    private UriInfo context;
+
+    public UbicacionWS() {
+    }
+    
+    
+    /* 
+    ######################################################
+    ######## registrar ubicación Empresa/sucursal ########
+    ######################################################
+     */
+    
+    @POST
+    @Path("registrarUbicacionEmpresa")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mensaje registrarUbicacionEmpresa(
+            @FormParam("calle") String calle, @FormParam("numero") Integer numero,
+            @FormParam("codigoPostal") String codigoPostal, @FormParam("ciudad") String ciudad,
+            @FormParam("idEmpresa") Integer idEmpresa) {
+
+        if (idEmpresa == null || idEmpresa <= 0) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (calle == null || calle.isEmpty()) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (numero == null) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (ciudad == null || ciudad.isEmpty()) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (codigoPostal == null || codigoPostal.isEmpty() || !Utilidades.validarCadena(codigoPostal, Utilidades.CODIGO_POSTAL_PATTERN)) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        Mensaje mensaje = UbicacionDAO.registrarUbicacionEmpresa(
+                calle, numero, codigoPostal, ciudad, idEmpresa);
+
+        return mensaje;
+    }
+    
+    
+      @POST
+    @Path("registrarUbicacionSucursal")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mensaje registrarUbicacionSucursal(
+            @FormParam("calle") String calle,
+            @FormParam("numero") Integer numero,
+            @FormParam("codigoPostal") String codigoPostal,
+            @FormParam("ciudad") String ciudad,
+            @FormParam("idSucursal") Integer idSucursal) {
+
+        if (idSucursal == null || idSucursal <= 0) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (calle == null || calle.isEmpty()) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (numero == null) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (ciudad == null || ciudad.isEmpty()) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (codigoPostal == null || codigoPostal.isEmpty() || !Utilidades.validarCadena(codigoPostal, Utilidades.CODIGO_POSTAL_PATTERN)) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        Mensaje mensaje = UbicacionDAO.registrarUbicacionSucursal(
+                calle, numero, codigoPostal, ciudad, idSucursal);
+
+        return mensaje;
+    }
+    
+    /* 
+    ######################################################
+    ######## editar/eliminar ubicación  ##################
+    ######################################################
+     */
+    
+    @PUT
+    @Path("editarUbicacion")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mensaje editarUbicacion(
+            @FormParam("calle") String calle, @FormParam("numero") Integer numero,
+            @FormParam("codigoPostal") String codigoPostal, @FormParam("ciudad") String ciudad,
+            @FormParam("idUbicacion") Integer idUbicacion) {
+
+        if (idUbicacion == null || idUbicacion <= 0) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (calle == null || calle.isEmpty()) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (numero == null) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (ciudad == null || ciudad.isEmpty()) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        if (codigoPostal == null || codigoPostal.isEmpty() || !Utilidades.validarCadena(codigoPostal, Utilidades.CODIGO_POSTAL_PATTERN)) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        Mensaje mensaje = UbicacionDAO.editarUbicacion(idUbicacion,calle, numero, codigoPostal, ciudad);
+
+        return mensaje;
+    }
+
+    @DELETE
+    @Path("eliminarUbicacion/{idUbicacion}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mensaje eliminarUbicacion(
+            @PathParam("idUbicacion") Integer idUbicacion) {
+
+        if (idUbicacion == null || idUbicacion <= 0) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        Mensaje mensaje = UbicacionDAO.eliminarUbicacion(idUbicacion);
+
+        return mensaje;
+    }    
+
+    
+}
