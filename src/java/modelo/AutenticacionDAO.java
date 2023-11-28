c53d9223eb22a662c15e102b868869ba143256db
Christian <filetelopezchristian@gmail.com>
2023-11-28 12:01:16 -0600

    Se implementó el método para poder logearse en la app mobile :smiling_face_with_three_hearts: :smiling_face_with_three_hearts:

--- src/java/modelo/AutenticacionDAO.java
+++ src/java/modelo/AutenticacionDAO.java
@@ -1,6 +1,7 @@
 package modelo;
 
 import java.util.HashMap;
+import modelo.pojo.Cliente;
 import modelo.pojo.Mensaje;
 import modelo.pojo.Usuario;
 import mybatis.MyBatisUtil;
@@ -13,34 +14,67 @@ public class AutenticacionDAO {
 
         Mensaje mensaje = new Mensaje();
         mensaje.setError(true);
-        
+
         SqlSession conexionBD = MyBatisUtil.getSession();
-        if(conexionBD != null){
+        if (conexionBD != null) {
             try {
-                HashMap<String,String> parametros = new HashMap<>();
+                HashMap<String, String> parametros = new HashMap<>();
                 parametros.put("userName", userName);
                 parametros.put("contrasenia", contrasenia);
-                
-                Usuario usuario = conexionBD.selectOne("autenticacion.loginEscritorio",parametros);
-                
-                if(usuario != null){
+
+                Usuario usuario = conexionBD.selectOne("autenticacion.loginEscritorio", parametros);
+
+                if (usuario != null) {
                     mensaje.setError(false);
-                    mensaje.setContenido("Bienvenid(@) "+ usuario.getNombre()+" al sistema de cupones.");
+                    mensaje.setContenido("Bienvenid(@) " + usuario.getNombre() + " al sistema de cupones.");
                     mensaje.setUsuarioSesion(usuario);
-                }else{
+                } else {
                     mensaje.setContenido("Usuario incorrecto y/o contraseña incorrectos, favor de verificar");
                 }
             } catch (Exception e) {
                 mensaje.setContenido("Error: " + e.getMessage());
-            }finally{
+            } finally {
                 conexionBD.close();
             }
-            
-        }else{
+
+        } else {
             mensaje.setContenido("Error: Por el momento no hay conexion en la base de datos");
         }
-        
+
         return mensaje;
     }
 
+    public static Mensaje iniciarSesionMobile(
+            String correo, String contrasenia) {
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(true);
+
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if (conexionBD != null) {
+            try {
+                HashMap<String, String> parametros = new HashMap<>();
+                parametros.put("correo", correo);
+                parametros.put("contrasenia", contrasenia);
+
+                Cliente cliente = conexionBD.selectOne("autenticacion.loginMobile", parametros);
+
+                if (cliente != null) {
+                    mensaje.setError(false);
+                    mensaje.setContenido("Bienvenid(@) " + cliente.getNombre() + " " + cliente.getApellidoPaterno() + " al sistema de cupones.");
+                    mensaje.setCliente(cliente);
+                } else {
+                    mensaje.setContenido("Correo incorrecto y/o contraseña incorrectos, favor de verificar");
+                }
+            } catch (Exception e) {
+                mensaje.setContenido("Error: " + e.getMessage());
+            } finally {
+                conexionBD.close();
+            }
+
+        } else {
+            mensaje.setContenido("Error: Por el momento no hay conexion en la base de datos");
+        }
+
+        return mensaje;
+    }
 }
