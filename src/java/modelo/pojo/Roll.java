7392d104397d92ba4d5c449088d8dfb29c93e979
Christian <filetelopezchristian@gmail.com>
2023-12-04 22:46:47 -0600

    Se implementó un nuevo pojo Roll para mandar los rolles

---
+++ src/java/modelo/pojo/Roll.java
@@ -0,0 +1,32 @@
+package modelo.pojo;
+
+public class Roll {
+
+    private Integer idRollUsuario;
+    private String roll;
+
+    public Roll() {
+    }
+    
+    public Roll(Integer idRollUsuario, String roll) {
+        this.idRollUsuario = idRollUsuario;
+        this.roll = roll;
+    }
+
+    public Integer getIdRollUsuario() {
+        return idRollUsuario;
+    }
+
+    public void setIdRollUsuario(Integer idRollUsuario) {
+        this.idRollUsuario = idRollUsuario;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+}
