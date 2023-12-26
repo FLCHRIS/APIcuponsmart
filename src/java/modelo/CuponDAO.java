bdfbd7c642d0059fd30751084447172fda7be426
aaronMan12 <111460458+aaronMan12@users.noreply.github.com>
2023-12-26 00:21:12 -0600

    uponws

--- src/java/modelo/CuponDAO.java
+++ src/java/modelo/CuponDAO.java
@@ -11,22 +11,22 @@ public class CuponDAO {
         Mensaje respuesta = new Mensaje();
         respuesta.setError(false);
         SqlSession conexionDB = mybatis.MyBatisUtil.getSession();
-        
-        if (conexionDB != null){
+
+        if (conexionDB != null) {
             try {
-                List<Promocion> promociones = conexionDB.selectList("buscarTodosLosCupones");
-                if (promociones.size() > 0){
+                List<Promocion> promociones = conexionDB.selectList("cupon.buscarTodosLosCupones");
+                if (promociones.size() > 0) {
                     respuesta.setContenido("Exito el la busqueda de los cupones");
                     respuesta.setPromociones(promociones);
-                }else{
+                } else {
                     respuesta.setError(true);
                     respuesta.setContenido("Error al obtener los cupones");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
                 respuesta.setError(true);
-                respuesta.setContenido("Error:"+ e);
-            }finally{
+                respuesta.setContenido("Error:" + e);
+            } finally {
                 conexionDB.close();
             }
         }
@@ -34,29 +34,57 @@ public class CuponDAO {
     }
 
     public static Mensaje buscarCuponesComercial(Integer idEmpresa) {
-         Mensaje respuesta = new Mensaje();
+        Mensaje respuesta = new Mensaje();
         respuesta.setError(false);
         SqlSession conexionDB = mybatis.MyBatisUtil.getSession();
-        
-        if (conexionDB != null){
+
+        if (conexionDB != null) {
             try {
-                List<Promocion> promociones = conexionDB.selectList("buscarCuponesComercial", idEmpresa);
-                if (promociones.size() > 0){
+                List<Promocion> promociones = conexionDB.selectList("cupon.buscarCuponesComercial", idEmpresa);
+                if (promociones.size() > 0) {
                     respuesta.setContenido("Exito el la busqueda de los cupones");
                     respuesta.setPromociones(promociones);
-                }else{
+                } else {
                     respuesta.setError(true);
                     respuesta.setContenido("Error al obtener los cupones");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
                 respuesta.setError(true);
-                respuesta.setContenido("Error:"+ e);
-            }finally{
+                respuesta.setContenido("Error:" + e);
+            } finally {
                 conexionDB.close();
             }
         }
         return respuesta;
     }
-    
+
+    public static Mensaje canjearCupon(String codigo) {
+        Mensaje respuesta = new Mensaje();
+        respuesta.setError(false);
+        SqlSession conexionDB = mybatis.MyBatisUtil.getSession();
+        
+        if (conexionDB != null){
+            try {
+                int filasAfectadas = conexionDB.update("cupon.canjearCupon", codigo);
+                
+                if (filasAfectadas != 0){
+                    respuesta.setContenido("Cupon canjeado con exito");
+                    conexionDB.commit();
+                }else{
+                    respuesta.setError(true);
+                    respuesta.setContenido("Canje del cupón invalido, verifique el código");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                respuesta.setError(true);
+                respuesta.setContenido("Error;"+e);
+                }finally {
+                conexionDB.close();
+            }
+        }
+        
+        return respuesta;
+    }
+
 }
--- src/java/ws/CuponWS.java
+++ src/java/ws/CuponWS.java
@@ -1,11 +1,15 @@
 package ws;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import modelo.CuponDAO;
 import modelo.pojo.Mensaje;
@@ -36,6 +40,19 @@ public class CuponWS {
         
         Mensaje mensaje = CuponDAO.buscarCuponesComercial(idEmpresa);
     
+        return mensaje;
+    }
+    
+    @PUT
+    @Path("canjearCupon")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mensaje canjearCupon(@FormParam("codigo") String codigo){
+        
+        if (codigo.length() != 8){
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+        
+        Mensaje mensaje = CuponDAO.canjearCupon(codigo);
         return mensaje;
     }
 }
